24edcb4305e9fe9dff980c8b8d46cfab36d7f361
甘邻龙 <jessiegan1987@163.com>
2017-04-24 17:36:29 +0800

    test

---
+++ ios/RCTBaiduMap/JDGAnnotationView.h
@@ -0,0 +1,14 @@
+//
+//  JDGAnnotaionView.h
+//  RCTBaiduMap
+//
+//  Created by JDG on 2017/4/17.
+//  Copyright © 2017年 lovebing.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <BaiduMapAPI_Map/BMKPinAnnotationView.h>
+#import <BaiduMapAPI_Map/BMKPointAnnotation.h>
+@interface JDGAnnotationView : BMKPinAnnotationView
+
+@end
