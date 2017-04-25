30f561187335b3a3bc724ec6549fdc46fe73a8f1
甘邻龙 <ganll@burnish.cn>
2017-04-25 17:17:06 +0800

    调整版本

---
+++ android/src/main/java/org/lovebing/reactnative/baidumap/CustomOverlay.java
@@ -0,0 +1,11 @@
+package com.letto.test.baidumapdemo.npm;
+
+import com.baidu.mapapi.map.Overlay;
+
+/**
+ * Created by Administrator. on 2017/4/25 0025.
+ */
+
+public class CustomOverlay extends Overlay{
+
+}
